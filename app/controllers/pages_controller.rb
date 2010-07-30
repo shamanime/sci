cc9082d77a881f92c28246fb33bff266d901b27d
Jefferson Queiroz Venerando <jefferson.design@gmail.com>
2010-07-30 16:45:28 -0300

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contato"
   end
+  
+  def about
+    @title = "Sobre"
+  end
+
 end
