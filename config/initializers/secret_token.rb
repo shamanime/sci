79c0e14c17944b28653e10bcd8767c897c4fa297
Jefferson Queiroz Venerando <jefferson.design@gmail.com>
2010-07-30 14:35:00 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sci::Application.config.secret_token = '7ee8464d5b511bd07e59068b24d255625ffec98489f3726da727ebaeae6ad7b77149444704ec33d1a4d6af6af1edb94b250defe6fcade032bf29f7dccf831a60'
