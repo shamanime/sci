13d27723e1e044ec91ee904b0337c9b295b2395e
Jefferson Queiroz Venerando <jefferson.design@gmail.com>
2010-08-03 11:29:43 -0300

    Added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,10 +26,13 @@ module SessionsHelper
     user == current_user
   end
   
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
   def deny_access
     store_location
-    flash[:notice] = "Please sign in to access this page."
-    redirect_to signin_path
+    redirect_to signin_path, :notice => "Please sign in to access this page."
   end
 
   def store_location
